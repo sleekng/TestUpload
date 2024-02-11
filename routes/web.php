4d135e6222110cb4c34310106499248cf1c42c47
Matthew Emmanuel <manuel.fecund@gmail.com>
2024-02-11 15:28:15 +0100

    Febuary 11-2

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+}); 
 
 Route::middleware([
     'auth:sanctum',
